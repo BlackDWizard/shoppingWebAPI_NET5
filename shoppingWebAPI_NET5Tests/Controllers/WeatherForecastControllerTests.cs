4240e5026ad21f4bbd77767420f837117106b746
JonathanHuang <jonathan520419@gmail.com>
2024-06-28 16:01:48 +0800

    gitignore edit files

---
+++ shoppingWebAPI_NET5Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using shoppingWebAPI_NET5.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingWebAPI_NET5.Controllers.Tests
+{
+    [()]
+    public class WeatherForecastControllerTests
+    {
+        [Test()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
